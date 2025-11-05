cd34ab326548126a544e188940e50c31197b9548
ibukun120 <ibukunoluwawomiloju@gmail.com>
2025-11-05 15:47:44 +0100

    fix5

--- app/components/Tin.tsx
+++ app/components/Tin.tsx
@@ -2,7 +2,7 @@
 
 const Tin = () => {
   return (
-    <div className="bg-[#0059FF] px-4 text-center text-sm text-white flex justify-center items-center py-3 gap-4 text-xl flex-col md:flex-row">
+    <div className="bg-[#0059FF] px-4 text-center text-sm text-white flex justify-center items-center py-3 gap-4 flex-col md:flex-row">
       <p>Designed for <span className="md:font-bold">fintech, edtech, logistics, and Saas</span> <span className="md:hidden">⭐</span></p>
       <span className="hidden md:inline">⭐</span>
       <p>Designed <span className="md:font-bold">landing pages, Mobile apps, Web Apps, Websites,</span></p>
