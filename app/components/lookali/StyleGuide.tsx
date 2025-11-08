e9225a82db2acaf91f4505a29f5194c0d4c22600
ibukun120 <ibukunoluwawomiloju@gmail.com>
2025-11-08 23:22:07 +0100

    fix6

--- app/components/lookali/StyleGuide.tsx
+++ app/components/lookali/StyleGuide.tsx
@@ -5,8 +5,8 @@ const StyleGuide = () => {
     <div className="bg-white px-6 md:px-16 lg:px-24 py-16 text-black">
       <h1 className="text-3xl font-bold text-green-600 mb-8">Style Guide</h1>
       <p className="text-xl font-semibold">Font</p>
-      <div className="flex justify-between">
-        <div className="text-2xl md:text-4xl lg:text-7xl">OUTFIT</div>
+      <div className="flex justify-between flex-col gap-6">
+        <div className="text-4xl lg:text-7xl mt-4 md:mt-0">OUTFIT</div>
 
         <div className="tracking-widest leading-6">
           <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
@@ -18,7 +18,7 @@ const StyleGuide = () => {
           </div>
         </div>
       </div>
-      <p className="text-xl font-semibold">Colours</p>
+      <p className="text-xl font-semibold mt-4 md:mt-0">Colours</p>
       {/* color */}
       <div className="flex">
         {/* green */}
